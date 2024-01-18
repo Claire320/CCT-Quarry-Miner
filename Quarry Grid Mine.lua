c775c1415f76038d73e23b623cc3b06b37eac7e7
Claire <114881222+Claire320@users.noreply.github.com>
2024-01-17 21:17:37 -0500

    Update Quarry Grid Mine.lua

--- Quarry Grid Mine.lua
+++ Quarry Grid Mine.lua
@@ -1,2 +1,31 @@
+local args = { ... }
+if #args ~= 3 then
+  print( "Usage: program <param1>" )
+  error()
+end
 
+local xDist,yDist,zDist = args[1],args[2],args[3]
 
+func fuelTurtle()
+  if turtle.getFuelLevel() ~= "unlimited" and turtle.getFuelLevel() < 1 then
+    turtle.refuel()
+  end
+end
+
+for y=0,yDist,1 do
+  for x=0,xDist,1 do
+    for z=0,zDist,1 do
+      fuelTurtle()
+      turtle.forward()
+    end
+    if x%2 == 0 then
+      turtle.turnRight()
+      turtle.forward()
+      turtle.turnRight()
+    else
+      turtle.turnLeft()
+      turtle.forward()
+      turtle.turnLeft()
+    end
+  end
+end
